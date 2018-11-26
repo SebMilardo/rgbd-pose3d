e77a32a0985342b661d77d8fd376cdbe9875a25c
Sebastiano Milardo <s.milardo@hotmail.it>
2018-11-26 14:39:51 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rgbd-pose3d",
+    version="0.0.1",
+    author="Sebastiano Milardo",
+    author_email="milardo@mit.edu",
+    description="3D Human Pose Estimation in RGBD Images for Robotic Task Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SebMilardo/rgbd-pose3d",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
